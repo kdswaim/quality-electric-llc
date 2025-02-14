53cf54e651a379e0fa3bae588d3ac513cfc13a48
Kristen Swaim <kswaimdev@gmail.com>
2025-02-14 15:09:49 -0500

    added compressor integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import compressor from "astro-compressor";
+
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://qualityelectricofindiana.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), partytown()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), partytown(), compressor()],
 });
