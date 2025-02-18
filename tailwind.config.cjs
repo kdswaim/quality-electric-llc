60ec8dd425f81e50148f45768a76f3d8712c86e1
Kristen Swaim <kswaimdev@gmail.com>
2025-02-18 15:59:24 -0500

    updated employees, switched grayscale effects on pictures, added color-fade-in effect to header and durations to else, updated phone numbers, added twitter metatag and favicon for apple, replaced broken symbols with svg icons, added phone to footer... I think that's all of it?

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      keyframes: {
+        fadeToColor: {
+          "0%": { filter: "grayscale(100%)" },
+          "100%": { filter: "grayscale(0%)" },
+        },
+      },
+      animation: {
+        fadeToColor: "fadeToColor 2.5s ease-in-out forwards",
+      },
       fontFamily: {
-        serif: [
-          "Quattrocento", 
-        ],
-        sans: [
-          "Belleza",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        serif: ["Quattrocento", "serif"],
+        sans: ["Belleza", "Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
